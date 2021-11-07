904f0f6eab0b3a560df6d59cdedefe3e6262b7e2
9joanna <jcohaonuna@gmail.com>
2021-11-07 22:16:54 +0800

    Initial Commit

---
+++ myeboo/myebooApp.swift
@@ -0,0 +1,17 @@
+//
+//  myebooApp.swift
+//  myeboo
+//
+//  Created by User04 on 2021/11/7.
+//
+
+import SwiftUI
+
+@main
+struct myebooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
